3e65d8cefb81e7a34c9d62be8527330f7bce5192
adekang <adekang@163.com>
2025-07-12 10:09:42 +0800

    你好

---
+++ index.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
+
+let a =1
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(port, host, () => {
+  console.log(`Server started: http://${host}:${port}`);
+});
